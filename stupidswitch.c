8c6969b4dab675f4518835b694518de3e8483752
Walker Aldridge <daldridge@lairdscomputer.com>
2024-06-29 12:16:18 -0500

    Tricks with switch 2

--- stupidswitch.c
+++ stupidswitch.c
@@ -2,13 +2,33 @@
 #include<stdio.h>
 
 int main(void) {
-    char i = 15;
+    char i = 2;
+
+    // Cursed
     switch(i)
         if(0) case 0: printf("Zero\n");
         else if(0) case 1: printf("One\n");
         else if(0) case 2: printf("Two\n");
         else if(0) case 15: printf("Fifteen\n");
         else if(0) default: printf("Something Else\n");
+
+    // Also cursed
+    switch(i) {
+        if(0) case 0: printf("Zero\n");
+        if(0) case 1: printf("One\n");
+        if(0) case 2: printf("Two\n");
+        if(0) case 15: printf("Fifteen\n");
+        if(0) default: printf("Something Else\n");
+    }
+
+    // Normal
+    switch(i) {
+        case 0: printf("Zero\n"); break;
+        case 1: printf("One\n"); break;
+        case 2: printf("Two\n"); break;
+        case 15: printf("Fifteen\n"); break;
+        default: printf("Something Else\n");
+    }
 
     return 0;
 }
