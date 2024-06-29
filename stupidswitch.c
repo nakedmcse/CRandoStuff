358c048dd56a5b413d9b13109e9840fd04f75f7a
Walker Aldridge <daldridge@lairdscomputer.com>
2024-06-29 09:33:31 -0500

    Tricks with switch

---
+++ stupidswitch.c
@@ -0,0 +1,14 @@
+//Stupid tricks with switch
+#include<stdio.h>
+
+int main(void) {
+    char i = 15;
+    switch(i)
+        if(0) case 0: printf("Zero\n");
+        else if(0) case 1: printf("One\n");
+        else if(0) case 2: printf("Two\n");
+        else if(0) case 15: printf("Fifteen\n");
+        else if(0) default: printf("Something Else\n");
+
+    return 0;
+}
