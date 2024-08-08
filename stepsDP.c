52c5fed476ceb7a7a5841f3cdd22581f5df4f55e
Walker Aldridge <daldridge@lairdscomputer.com>
2024-08-08 15:24:54 -0500

    Step counter Dynamic Programming

--- stepsDP.c
+++ stepsDP.c
@@ -1,15 +1,39 @@
 //  Figure out steps path for 1 or 2 steps on stair size 1->n->MAXSTEPS
 #include<stdio.h>
-#define MAXSTEPS 5
+#include<time.h>
+#define MAXSTEPS 45
 
-int checkSteps(int step) {
+// Recursive Solution
+int checkStepsRecursive(int step) {
     if(step > MAXSTEPS) return 0;   // End Case - bust
     if(step == MAXSTEPS) return 1;  // End Case - valid solution
-    return checkSteps(step+1) + checkSteps(step+2);
+    return checkStepsRecursive(step+1) + checkStepsRecursive(step+2);
+}
+
+// Dynamic Programming Solution
+int checkStepsDP() {
+    int steps[MAXSTEPS+1] = {0};
+
+    steps[0] = 1;  // One way for no steps
+    steps[1] = 1;  // One way for one step
+
+    for(int i=2; i<=MAXSTEPS; i++) {
+        steps[i] = steps[i-1] + steps[i-2];  // step i can be reached from 1 step before or 2 steps
+    }
+
+    return steps[MAXSTEPS];
 }
 
 int main(void) {
-    int solCount = checkSteps(0);
-    printf("%d solutions\n", solCount);
+    clock_t start_time = clock();
+    int solCountR = checkStepsRecursive(0);
+    double elapsedR = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+
+    start_time = clock();
+    int solCountDP = checkStepsDP();
+    double elapsedDP = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+
+    printf("%d steps\n", MAXSTEPS);
+    printf("%d recursive solutions in %fs, %d DP solutions in %fs\n", solCountR, elapsedR, solCountDP, elapsedDP);
     return 0;
 }
