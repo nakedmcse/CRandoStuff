86d8184d601a3b2f941f2962a32cef4045cd302f
Walker Aldridge <daldridge@lairdscomputer.com>
2024-08-08 09:12:07 -0500

    Step counter

---
+++ stepsDP.c
@@ -0,0 +1,16 @@
+//  Use Dynamic Programming to figure out steps path for 1 or 2 steps on stair size 1->n->MAXSTEPS
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXSTEPS 5
+
+int checkSteps(int step) {
+    if(step > MAXSTEPS) return 0;   // End Case - bust
+    if(step == MAXSTEPS) return 1;  // End Case - valid solution
+    return checkSteps(step+1) + checkSteps(step+2);
+}
+
+int main(void) {
+    int solCount = checkSteps(0);
+    printf("%d solutions\n", solCount);
+    return 0;
+}
