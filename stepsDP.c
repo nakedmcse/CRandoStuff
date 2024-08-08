ace6568ab58ba4c0f88b5c78a3b655edf6b784ec
Walker Aldridge <daldridge@lairdscomputer.com>
2024-08-08 09:13:52 -0500

    Step counter

--- stepsDP.c
+++ stepsDP.c
@@ -1,6 +1,5 @@
 //  Use Dynamic Programming to figure out steps path for 1 or 2 steps on stair size 1->n->MAXSTEPS
 #include<stdio.h>
-#include<stdlib.h>
 #define MAXSTEPS 5
 
 int checkSteps(int step) {
