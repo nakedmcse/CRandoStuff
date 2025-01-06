a13f4c64260d01cae0aeaa1b3955b6990791b590
Walker Aldridge <daldridge@lairdscomputer.com>
2025-01-05 21:53:49 -0600

    Stupid copy

--- bakastringcopy.c
+++ bakastringcopy.c
@@ -5,15 +5,15 @@ void bakaCopy(char *to, char *from, size_t count) {
     size_t n = (count + 7) / 8;
 
     switch (count % 8) {
-        case 0: printf("Case 0 copy\n"); do {
-            *to++ = *from++;
-            case 7: printf("Case 7 copy\n"); *to++ = *from++;
-            case 6: printf("Case 6 copy\n"); *to++ = *from++;
-            case 5: printf("Case 5 copy\n"); *to++ = *from++;
-            case 4: printf("Case 4 copy\n"); *to++ = *from++;
-            case 3: printf("Case 3 copy\n"); *to++ = *from++;
-            case 2: printf("Case 2 copy\n"); *to++ = *from++;
-            case 1: printf("Case 1 copy\n"); *to++ = *from++;
+        case 0:  do {
+            printf("Case 0 copy, %ld\n", n); *to++ = *from++;
+            case 7: printf("Case 7 copy, %ld\n", n); *to++ = *from++;
+            case 6: printf("Case 6 copy, %ld\n", n); *to++ = *from++;
+            case 5: printf("Case 5 copy, %ld\n", n); *to++ = *from++;
+            case 4: printf("Case 4 copy, %ld\n", n); *to++ = *from++;
+            case 3: printf("Case 3 copy, %ld\n", n); *to++ = *from++;
+            case 2: printf("Case 2 copy, %ld\n", n); *to++ = *from++;
+            case 1: printf("Case 1 copy, %ld\n", n); *to++ = *from++;
         } while (--n > 0);
     }
 }
